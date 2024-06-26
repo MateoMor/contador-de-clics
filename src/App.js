5fcda06dabbf4f17d6f632b3778430c045a45f9d
MateoMor <mateomorales5558@gmail.com>
2024-06-25 23:54:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Boton from "./components/Boton";
+import Contador from "./components/Contador";
+import asteroid from "./imgs/asteroid.png";
+import { useState } from "react";
 
 function App() {
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics((prevNumClics) => prevNumClics + 1);
+
+    const contador = document.querySelector("#contador-contenedor");
+    if (contador && !contador.classList.contains("animar")) {
+      contador.classList.add("animar");
+      setTimeout(() => {
+        contador.classList.remove("animar");
+      }, 300);
+    }
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-principal">
+        <Contador numClics={numClics} />
+        <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
     </div>
   );
 }
